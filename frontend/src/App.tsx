e7bfd48a00f95cc49370064cfc41e488c3dae285
faizal <faisalt661@gmail.com>
2025-02-22 23:57:47 +0530

    feat: integrate Tailwind CSS into the project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <h1>Starting project</h1>
+      <h1 className="text-3xl font-bold text-center pt-20 text-violet-800 ">Starting project</h1>
     </>
   )
 }
