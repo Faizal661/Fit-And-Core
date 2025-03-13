740e8f07c097b28452d340847d8cd29792b9da7c
faizal <faisalt661@gmail.com>
2025-03-13 21:42:45 +0530

    feat: access token and refresh token create

---
+++ backend/src/utils/token.util.ts
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import { IUserModel } from "../models/user.models";
+dotenv.config();
+
+const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || "";
+const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET || "";
+
+export const generateAccessToken = (user: IUserModel) => {
+  return jwt.sign(
+    { id: user._id, email: user.email, role: user.role },
+    ACCESS_TOKEN_SECRET,
+    { expiresIn: "15m" }
+  );
+};
+
+export const generateRefreshToken = (user: IUserModel) => {
+  return jwt.sign(
+    { id: user._id, email: user.email, role: user.role },
+    REFRESH_TOKEN_SECRET,
+    { expiresIn: "7d" }
+  );
+};
