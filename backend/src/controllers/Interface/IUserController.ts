bd149fd1ce40353281a67554cf53629229d588a8
faizal <faisalt661@gmail.com>
2025-03-31 01:45:15 +0530

    wip : users manage section in admin with pagination, searching.

--- backend/src/controllers/Interface/IUserController.ts
+++ backend/src/controllers/Interface/IUserController.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 
 export interface IUserController {
+  getUsers(req: Request, res: Response, next: NextFunction): Promise<void>;
+  toggleBlockStatus(req: Request, res: Response, next: NextFunction): Promise<void>;
   getUserProfile(req: Request, res: Response, next: NextFunction): Promise<void>;
   updateUserProfile(req: Request, res: Response, next: NextFunction): Promise<void>;
   updateProfilePicture(req: Request, res: Response, next: NextFunction): Promise<void>; 
