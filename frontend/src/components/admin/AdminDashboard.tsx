9a6417cf8527c3de2ee6761d476581fd810ab7b7
faizal <faisalt661@gmail.com>
2025-04-05 17:57:18 +0530

    style : add loading skeleton

--- frontend/src/components/admin/AdminDashboard.tsx
+++ frontend/src/components/admin/AdminDashboard.tsx
@@ -24,8 +24,13 @@ import {
   fetchTrainerCount,
   fetchMonthlyRegistrationData,
 } from "../../services/admin/adminDashboard";
+import Skeleton from '@mui/material/Skeleton';
 
+ 
 const subscriptionData = [
+  { name: "Jan", users: 0, trainers: 0 },
+  { name: "Feb", users: 0, trainers: 0 },
+  { name: "Mar", users: 0, trainers: 0 },
   { name: "Apr", users: 0, trainers: 0 },
   { name: "May", users: 0, trainers: 0 },
   { name: "Jun", users: 0, trainers: 0 },
@@ -46,12 +51,16 @@ const MetricCard = ({
   currentMonthValue,
   change,
   icon: Icon,
+  isLoading,
+  isError,
 }: {
   title: string;
   totalValue: string;
   currentMonthValue: string;
   change: number;
   icon: LucideIcon;
+  isLoading: boolean;
+  isError: Error | null;
 }) => {
   const isPositive = change >= 0;
 
@@ -61,25 +70,47 @@ const MetricCard = ({
         {Icon && <Icon size={16} className="mr-2" />}
         {title}
       </div>
-      <div className="text-2xl font-bold mb-1">{totalValue}</div>
-      {currentMonthValue !== undefined && (
-        <div className="text-sm text-gray-600 mb-1">
-          This Month: {currentMonthValue}
+      {isError ? (
+        <div className="text-red-600 text-xl font-semibold my-6">Failed to fetch data.</div> 
+      ) : (
+        <div>
+          <div className="text-2xl font-bold mb-1">
+            {isLoading ? (
+              <Skeleton
+                width={30}
+                animation="wave"
+              />
+            ) : (
+              totalValue
+            )}
+          </div>
+          {currentMonthValue !== undefined && (
+            <div className="text-sm text-gray-600 mb-1">
+              This Month:{" "}
+              {isLoading ? <Skeleton width={40} /> : currentMonthValue}
+            </div>
+          )}
+          <div
+            className={`text-xs ${
+              isPositive ? "text-green-500" : "text-red-500"
+            } flex items-center`}
+          >
+            {isLoading ? (
+              <Skeleton width={150} />
+            ) : (
+              <>
+                {isPositive ? (
+                  <ArrowUp size={12} className="mr-1" />
+                ) : (
+                  <ArrowDown size={12} className="mr-1" />
+                )}
+                {Math.abs(change || 0)}% {isPositive ? "increase" : "decrease"}{" "}
+                from last month
+              </>
+            )}
+          </div>
         </div>
       )}
-      <div
-        className={`text-xs ${
-          isPositive ? "text-green-500" : "text-red-500"
-        } flex items-center`}
-      >
-        {isPositive ? (
-          <ArrowUp size={12} className="mr-1" />
-        ) : (
-          <ArrowDown size={12} className="mr-1" />
-        )}
-        {Math.abs(change)}% {isPositive ? "increase" : "decrease"} from last
-        month
-      </div>
     </div>
   );
 };
@@ -107,43 +138,13 @@ const AdminDashboard = () => {
 
   const {
     data: graphData,
-    // isLoading: isLoadingMonthly,
+    isLoading: isLoadingMonthly,
     // error: monthlyError,
   } = useQuery({
     queryKey: ["monthlyRegistrations"],
     queryFn: fetchMonthlyRegistrationData,
     staleTime: 5 * 60 * 1000,
   });
-
-  if (isLoadingUsers || isLoadingTrainers) {
-    return (
-      <>
-        <h1 className="text-2xl font-bold pl-20 mb-3 px-6 pt-4 ">DASHBOARD</h1>
-        <div className="border-b-1 pt-2 mb-5"></div>
-        <div className="h-96  flex justify-center items-center text-4xl font-bold">
-          <div className="text-center ">
-            <span className="ml-2">Loading dashboard data...</span>
-          </div>
-        </div>
-      </>
-    );
-  }
-
-  if (userError || trainerError) {
-    return (
-      <>
-        <h1 className="text-2xl font-bold pl-20 mb-3 px-6 pt-4 ">DASHBOARD</h1>
-        <div className="border-b-1 pt-2 mb-5"></div>
-        <div className="h-60  flex justify-center items-center text-4xl text-red-700 font-bold">
-          <div className="text-center ">
-            <span className="ml-2">
-              Failed to load dashboard data. Please try again later.
-            </span>
-          </div>
-        </div>
-      </>
-    );
-  }
 
   return (
     <div className="">
@@ -158,6 +159,8 @@ const AdminDashboard = () => {
           currentMonthValue="₹0"
           change={0}
           icon={CreditCard}
+          isLoading={false}
+          isError={null}
         />
 
         {/* Refund Card */}
@@ -167,6 +170,8 @@ const AdminDashboard = () => {
           currentMonthValue="₹0"
           change={0}
           icon={RefreshCcw}
+          isLoading={false}
+          isError={null}
         />
 
         {/* Income Card */}
@@ -176,53 +181,64 @@ const AdminDashboard = () => {
           currentMonthValue="₹0"
           change={0}
           icon={CreditCard}
+          isLoading={false}
+          isError={null}
         />
       </div>
 
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 px-6 py-4">
         {/* Chart */}
+
         <div className="lg:col-span-2 bg-white rounded-lg shadow-md p-4">
           <h2 className="text-lg font-semibold mb-4">New Registrations</h2>
-          <div className="h-80">
-            <ResponsiveContainer width="100%" height="100%">
-              <LineChart data={graphData?.monthlyData || subscriptionData}>
-                <CartesianGrid strokeDasharray="3 3" vertical={false} />
-                <XAxis dataKey="name" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Line
-                  type="monotone"
-                  dataKey="users"
-                  stroke="#2D31FA"
-                  strokeWidth={2}
-                  activeDot={{ r: 8 }}
-                />
-                <Line
-                  type="monotone"
-                  dataKey="trainers"
-                  stroke="#51C1CB"
-                  strokeWidth={2}
-                />
-              </LineChart>
-            </ResponsiveContainer>
-          </div>
+          {isLoadingMonthly ? (
+            <Skeleton height={300} variant="rounded" animation='wave' />
+          ) : (
+            <div className="h-80">
+              <ResponsiveContainer width="100%" height="100%">
+                <LineChart data={graphData?.monthlyData || subscriptionData}>
+                  <CartesianGrid strokeDasharray="3 3" vertical={false} />
+                  <XAxis dataKey="name" />
+                  <YAxis />
+                  <Tooltip />
+                  <Legend />
+                  <Line
+                    type="monotone"
+                    dataKey="users"
+                    stroke="#2D31FA"
+                    strokeWidth={2}
+                    activeDot={{ r: 8 }}
+                  />
+                  <Line
+                    type="monotone"
+                    dataKey="trainers"
+                    stroke="#51C1CB"
+                    strokeWidth={2}
+                  />
+                </LineChart>
+              </ResponsiveContainer>
+            </div>
+          )}
         </div>
 
         <div className="space-y-6">
           <MetricCard
             title="Total Users"
-            totalValue={totalUserData?.totalCount || "0"}
+            totalValue={totalUserData?.totalCount}
             currentMonthValue={totalUserData?.currentMonthCount || "0"}
             change={totalUserData?.percentChange || 0}
             icon={Users}
+            isLoading={isLoadingUsers}
+            isError={userError}
           />
           <MetricCard
             title="Total Trainers"
-            totalValue={totalTrainerData?.totalCount || "0"}
+            totalValue={totalTrainerData?.totalCount}
             currentMonthValue={totalTrainerData?.currentMonthCount || "0"}
             change={totalTrainerData?.percentChange || 0}
             icon={Dumbbell}
+            isLoading={isLoadingTrainers}
+            isError={trainerError}
           />
           {/* <MetricCard
             title="Active Users"
